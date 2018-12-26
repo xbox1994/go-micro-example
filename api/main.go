f623dd699389c759df3df051c0d849a4519bda4c
Tianyi Wang <tywang666@gmail.com>
2018-12-26 16:16:36 +0800

    简化api构建

--- api/main.go
+++ api/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"GoMicroExample/api/auth"
 	"github.com/micro/micro/cmd"
+	"github.com/micro/micro/plugin"
 )
+
+func init() {
+	plugin.Register(&auth.Auth{})
+}
 
 func main() {
 	cmd.Init()
